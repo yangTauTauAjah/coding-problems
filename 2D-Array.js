6830beefb6ecf9416dd14414c9031ff79a7f334a
yangTauTauAjah <93625104+yangTauTauAjah@users.noreply.github.com>
2023-02-05 07:26:38 +0700

    Create 2D-Array.js

---
+++ 2D-Array.js
@@ -0,0 +1,23 @@
+let result = []
+
+for (let i = 0; i < (inputMatrix.length - 2); i++) {
+  
+  let row = []
+  
+  for (let j = 0; j < (inputMatrix[i].length - 2); j++) {
+    
+    row.push(
+        inputMatrix[i+0][j+0]
+      + inputMatrix[i+0][j+1]
+      + inputMatrix[i+0][j+2]
+      + inputMatrix[i+1][j+1]
+      + inputMatrix[i+2][j+0]
+      + inputMatrix[i+2][j+1]
+      + inputMatrix[i+2][j+2]
+    )
+    
+  }
+  
+  result.push(row)
+  
+}
